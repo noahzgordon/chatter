4db51d9b67fc6668e61f2411aeef9cff1f5f9eb4
Daniel+Noah <Daniel+Noah@appacademy.io>
2014-08-25 15:13:08 -0400

    Add list of users to index page.

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -4,11 +4,14 @@
   var ChatUI = ChatApp.ChatUI = function(chat, $el) {
     this.chat = chat;
     this.$el = $el;
+    this.nicknames = {};
+    console.log(this.chat.socket)
 
     var ui = this;
     this.chat.socket.on('message', ui.renderMessage.bind(ui));
     this.chat.socket.on('nicknameChangeResult', ui.renderMessage.bind(ui))
     this.$el.find("form.new-message").on("submit", ui.getMessage.bind(ui));
+    this.chat.socket.on('nicknamesUpdate', ui.renderNicknames.bind(ui));
   }
 
   ChatUI.prototype.getMessage = function(event) {
@@ -20,13 +23,25 @@
     } else {
       this.chat.sendMessage(data);
     }
+
+    $(event.currentTarget).find('#new-message-text').val("");
   }
 
   ChatUI.prototype.renderMessage = function(data) {
-    console.log(data)
     var $li = $("<li>");
     $li.text(data.text);
     this.$el.find("ul.chat").append($li);
+  }
+
+  ChatUI.prototype.renderNicknames = function(data) {
+    this.$el.find("ul.users").empty();
+    var $li;
+    var that = this;
+
+    Object.keys(data.nicknames).forEach(function(key) {
+      $li = $("<li>").text(data.nicknames[key]);
+      that.$el.find("ul.users").append($li);
+    });
   }
 
 
